16c6a6b5e80bc66877c713bf8c7b50772fa68b25
--replace-all <surajprofile27@gmail.com>
2025-07-12 06:48:47 +0530

    prepared for the deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const BASE_URl =
+  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: BASE_URl,
 });
 
 export default api;
